ba6c2f94d0082914007285a80192081aef49e5ad
Albert <abeiphone@bk.ru>
2024-05-19 13:33:00 +0300

    redeploy

--- src/bot.ts
+++ src/bot.ts
@@ -1,302 +1,315 @@
-import { RemindersGame, formatedDate, formatedTime } from "./utils";
-import { commands } from "./data";
-import { IGame, IHead2Head, IScorer, ISquad, IStandings } from "./types";
-import { Bot, Context, NextFunction, webhookCallback } from "grammy";
-import { Router } from "express";
-import express from "express";
-import fetch from "node-fetch";
+import { RemindersGame, formatedDate, formatedTime } from './utils'
+import { commands } from './data'
+import { IGame, IHead2Head, IScorer, ISquad, IStandings } from './types'
+import { Bot, Context, NextFunction, webhookCallback } from 'grammy'
+import { Router } from 'express'
+import express from 'express'
+import fetch from 'node-fetch'
 
 // Create a bot using the Telegram token
-const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
+const bot = new Bot(process.env.TELEGRAM_TOKEN || '')
 // Suggest commands in the menu
-bot.api.setMyCommands(commands);
+bot.api.setMyCommands(commands)
 
 // global variable for the request head2head
-let game: IGame | undefined;
-let headId: number | undefined;
-let previousId: number | undefined;
+let game: IGame | undefined
+let headId: number | undefined
+let previousId: number | undefined
 
-const today = new Date().toISOString().split("T")[0];
+const today = new Date().toISOString().split('T')[0]
 // Handle the /start command to greet the user 00 00 12 * * 0-6
 
-bot.command("start", (ctx) => {
-  const name = ctx.from?.first_name;
-  if (ctx.from?.username === "Azamat_dzagoi") {
-    return ctx.reply(
-      `Салам Гады, Манан ус най, манан похуй у, доместоса хугаева 10 или 12`
-    );
-  }
-  if (ctx.from?.username === "MaratEsiev") {
-    return ctx.reply(`Салам кинконганенок шыта факусыс?, индонезия китай`);
-  }
-  ctx.reply(`Здраствуйте ${name} 🫡, это Бот с календарем игр Ювентуса
-  \nHello ${name} 🫡, this is a Bot with the Juventus games calendar`);
-});
+bot.command('start', ctx => {
+	const name = ctx.from?.first_name
+	if (ctx.from?.username === 'Azamat_dzagoi') {
+		ctx.reply(
+			`Салам Гады, Манан ус най, манан похуй у, доместоса хугаева 10 или 12`
+		)
+	}
+	if (ctx.from?.username === 'MaratEsiev') {
+		ctx.reply(`Салам кинконганенок шыта факусыс?, индонезия китай`)
+	}
+	ctx.reply(`  
+  Hello ${name} 🫡, this is a Bot with the Juventus games calendar
 
-bot.command("nextgame", async (ctx) => {
-  try {
-    if (!game?.matchday) {
-      const options = {
-        headers: { "X-Auth-Token": "1bb65d5d077f4ccba1280a3735cb9242" },
-      };
+Games
+ /previousgame - show the previous game
+ /nextgame - show the next game
 
-      await fetch(
-        `https://api.football-data.org/v4/teams/109/matches?dateFrom=${today}&dateTo=2024-09-29&?limit=1`,
-        options
-      )
-        .then((response) => response.json())
-        .then((response) => (game = response.matches[0]));
-    }
+Standings
+ /standings - show the standings Serie A
+ /scorers - show the standings scorers Serie A
+ 
+Squad 
+ /squad - show the team composition
 
-    headId = game?.id;
+* data taken from API football-data.org and are not always relevant
+`)
+})
 
-    previousId =
-      game?.awayTeam.name !== "Juventus FC"
-        ? game?.awayTeam.id
-        : game?.homeTeam.id;
+bot.command('nextgame', async ctx => {
+	try {
+		if (!game?.matchday) {
+			const options = {
+				headers: { 'X-Auth-Token': '1bb65d5d077f4ccba1280a3735cb9242' },
+			}
 
-    const away = game?.awayTeam;
-    const home = game?.homeTeam;
-    const date = game?.utcDate.toString().split("T")[0];
-    const time = game?.utcDate.toString().split("T")[1].substring(0, 5);
+			await fetch(
+				`https://api.football-data.org/v4/teams/109/matches?dateFrom=${today}&dateTo=2024-09-29&?limit=1`,
+				options
+			)
+				.then(response => response.json())
+				.then(response => (game = response.matches[0]))
+		}
 
-    const message = `${game?.competition.name} - ${
-      game?.matchday
-    } тур\n\n${formatedDate(date)} --- ${formatedTime(time)}\n${home?.name} - ${
-      away?.name
-    }\n\nОтправьте /head2head что бы посмотреть историю противостояния этих команд \n\nОтправьте /previous что бы посмотреть как сыграла команда противника`;
+		headId = game?.id
 
-    return ctx.reply(message);
-  } catch (error) {
-    ctx.reply(`Что то пошло не так, повторите попытку позже`);
-  }
-});
+		previousId =
+			game?.awayTeam.name !== 'Juventus FC'
+				? game?.awayTeam.id
+				: game?.homeTeam.id
 
-bot.command("previousgame", async (ctx) => {
-  try {
-    let game: IGame | undefined;
+		const away = game?.awayTeam
+		const home = game?.homeTeam
+		const date = game?.utcDate.toString().split('T')[0]
+		const time = game?.utcDate.toString().split('T')[1].substring(0, 5)
 
-    if (!game?.matchday) {
-      const options = {
-        headers: { "X-Auth-Token": "1bb65d5d077f4ccba1280a3735cb9242" },
-      };
+		const message = `${game?.competition.name} - ${
+			game?.matchday
+		} тур\n\n${formatedDate(date)} --- ${formatedTime(time)}\n${home?.name} - ${
+			away?.name
+		}\n\nОтправьте /head2head что бы посмотреть историю противостояния этих команд \n\nОтправьте /previous что бы посмотреть как сыграла команда противника`
 
-      await fetch(
-        `https://api.football-data.org/v4/teams/109/matches?dateFrom=2023-08-29&?${today}&dateTo=${today}&?limit=1`,
-        options
-      )
-        .then((response) => response.json())
-        .then(
-          (response) => (game = response.matches[response.matches.length - 1])
-        );
-    }
+		return ctx.reply(message)
+	} catch (error) {
+		ctx.reply(`Что то пошло не так, повторите попытку позже`)
+	}
+})
 
-    if (game?.id) {
-      const { away, home } = game?.score.fullTime;
-      const { matchday, awayTeam, competition, homeTeam, utcDate } = game;
+bot.command('previousgame', async ctx => {
+	try {
+		let game: IGame | undefined
 
-      const message = `${competition.name} - ${matchday} тур\n${formatedDate(
-        utcDate.toString().split("T")[0]
-      )}\n${homeTeam.name} ${home} - ${away} ${awayTeam.name}`;
+		if (!game?.matchday) {
+			const options = {
+				headers: { 'X-Auth-Token': '1bb65d5d077f4ccba1280a3735cb9242' },
+			}
 
-      return ctx.reply(message);
-    }
-  } catch (error) {
-    ctx.reply(`Что то пошло не так, повторите попытку позже`);
-  }
-});
+			await fetch(
+				`https://api.football-data.org/v4/teams/109/matches?dateFrom=2023-08-29&?${today}&dateTo=${today}&?limit=1`,
+				options
+			)
+				.then(response => response.json())
+				.then(
+					response => (game = response.matches[response.matches.length - 1])
+				)
+		}
 
-bot.command("scorers", async (ctx) => {
-  try {
-    let player: IScorer[] = [];
+		if (game?.id) {
+			const { away, home } = game?.score.fullTime
+			const { matchday, awayTeam, competition, homeTeam, utcDate } = game
 
-    if (!player.length) {
-      const options = {
-        headers: { "X-Auth-Token": "1bb65d5d077f4ccba1280a3735cb9242" },
-      };
+			const message = `${competition.name} - ${matchday} тур\n${formatedDate(
+				utcDate.toString().split('T')[0]
+			)}\n${homeTeam.name} ${home} - ${away} ${awayTeam.name}`
 
-      await fetch(
-        "https://api.football-data.org/v4/competitions/SA/scorers?limit=10",
-        options
-      )
-        .then((response) => response.json())
-        .then((response) => (player = response.scorers));
-    }
+			return ctx.reply(message)
+		}
+	} catch (error) {
+		ctx.reply(`Что то пошло не так, повторите попытку позже`)
+	}
+})
 
-    const message = player?.reduce((acc, scorer) => {
-      return (
-        acc +
-        `${scorer.player.name} ${scorer.goals} - ${scorer.assists || 0} (${
-          scorer.team.shortName
-        })\n`
-      );
-    }, "");
+bot.command('scorers', async ctx => {
+	try {
+		let player: IScorer[] = []
 
-    return ctx.reply(message);
-  } catch (error) {
-    ctx.reply(`Что то пошло не так, повторите попытку позже`);
-  }
-});
+		if (!player.length) {
+			const options = {
+				headers: { 'X-Auth-Token': '1bb65d5d077f4ccba1280a3735cb9242' },
+			}
 
-bot.command("standings", async (ctx) => {
-  try {
-    let table: IStandings[] = [];
+			await fetch(
+				'https://api.football-data.org/v4/competitions/SA/scorers?limit=10',
+				options
+			)
+				.then(response => response.json())
+				.then(response => (player = response.scorers))
+		}
 
-    if (!table.length) {
-      const options = {
-        headers: { "X-Auth-Token": "1bb65d5d077f4ccba1280a3735cb9242" },
-      };
+		const message = player?.reduce((acc, scorer) => {
+			return (
+				acc +
+				`${scorer.player.name} ${scorer.goals} - ${scorer.assists || 0} (${
+					scorer.team.shortName
+				})\n`
+			)
+		}, '')
 
-      await fetch(
-        "https://api.football-data.org/v4/competitions/SA/standings",
-        options
-      )
-        .then((response) => response.json())
-        .then((response) => (table = response.standings[0].table));
-    }
+		return ctx.reply(message)
+	} catch (error) {
+		ctx.reply(`Что то пошло не так, повторите попытку позже`)
+	}
+})
 
-    const message = table?.reduce(
-      (acc, team) =>
-        acc +
-        `${team.team.tla} - ${team.points} pts ${team.playedGames} games\n`,
-      ""
-    );
+bot.command('standings', async ctx => {
+	try {
+		let table: IStandings[] = []
 
-    return ctx.reply(message);
-  } catch (error) {
-    ctx.reply(`Что то пошло не так, повторите попытку позже`);
-  }
-});
+		if (!table.length) {
+			const options = {
+				headers: { 'X-Auth-Token': '1bb65d5d077f4ccba1280a3735cb9242' },
+			}
 
-bot.command("squad", async (ctx) => {
-  try {
-    let squad: ISquad[] = [];
-    let message = "";
+			await fetch(
+				'https://api.football-data.org/v4/competitions/SA/standings',
+				options
+			)
+				.then(response => response.json())
+				.then(response => (table = response.standings[0].table))
+		}
 
-    if (!squad.length) {
-      const options = {
-        headers: { "X-Auth-Token": "1bb65d5d077f4ccba1280a3735cb9242" },
-      };
+		const message = table?.reduce(
+			(acc, team) =>
+				acc +
+				`${team.team.tla} - ${team.points} pts ${team.playedGames} games\n`,
+			''
+		)
 
-      await fetch("https://api.football-data.org/v4/teams/109", options)
-        .then((response) => response.json())
-        .then((response) => (squad = response.squad));
-    }
+		return ctx.reply(message)
+	} catch (error) {
+		ctx.reply(`Что то пошло не так, повторите попытку позже`)
+	}
+})
 
-    for (let i = 0; i < squad.length; i++) {
-      if (squad[i]?.position !== squad[i - 1]?.position) {
-        message += `\n<u>${squad[i].position}</u>\n`;
-      }
-      message += `${squad[i].name}\n`;
-    }
+bot.command('squad', async ctx => {
+	try {
+		let squad: ISquad[] = []
+		let message = ''
 
-    return ctx.reply(message, { parse_mode: "HTML" });
-  } catch (error) {
-    ctx.reply(`Что то пошло не так, повторите попытку позже`);
-  }
-});
+		if (!squad.length) {
+			const options = {
+				headers: { 'X-Auth-Token': '1bb65d5d077f4ccba1280a3735cb9242' },
+			}
+
+			await fetch('https://api.football-data.org/v4/teams/109', options)
+				.then(response => response.json())
+				.then(response => (squad = response.squad))
+		}
+
+		for (let i = 0; i < squad.length; i++) {
+			if (squad[i]?.position !== squad[i - 1]?.position) {
+				message += `\n<u>${squad[i].position}</u>\n`
+			}
+			message += `${squad[i].name}\n`
+		}
+
+		return ctx.reply(message, { parse_mode: 'HTML' })
+	} catch (error) {
+		ctx.reply(`Что то пошло не так, повторите попытку позже`)
+	}
+})
 
 const defaultReply = async (ctx: Context, next: NextFunction) => {
-  if (ctx.message?.text === "/head2head") {
-    let head: IHead2Head | undefined;
+	if (ctx.message?.text === '/head2head') {
+		let head: IHead2Head | undefined
 
-    if (headId) {
-      const options = {
-        headers: { "X-Auth-Token": "1bb65d5d077f4ccba1280a3735cb9242" },
-      };
+		if (headId) {
+			const options = {
+				headers: { 'X-Auth-Token': '1bb65d5d077f4ccba1280a3735cb9242' },
+			}
 
-      await fetch(
-        `https://api.football-data.org/v4/matches/${headId}/head2head`,
-        options
-      )
-        .then((response) => response.json())
-        .then((response) => (head = response));
-    }
+			await fetch(
+				`https://api.football-data.org/v4/matches/${headId}/head2head`,
+				options
+			)
+				.then(response => response.json())
+				.then(response => (head = response))
+		}
 
-    if (head) {
-      const { homeTeam, awayTeam, numberOfMatches } = head?.aggregates;
-      const juve = homeTeam.name === "Juventus FC" ? homeTeam : awayTeam;
-      const anotherTeam = homeTeam.name !== "Juventus FC" ? homeTeam : awayTeam;
+		if (head) {
+			const { homeTeam, awayTeam, numberOfMatches } = head?.aggregates
+			const juve = homeTeam.name === 'Juventus FC' ? homeTeam : awayTeam
+			const anotherTeam = homeTeam.name !== 'Juventus FC' ? homeTeam : awayTeam
 
-      const result = `В последних ${numberOfMatches} матчах у Юве против ${anotherTeam.name} \n${juve.wins} побед ${juve.draws} ничей ${juve.losses} поражений `;
-      return await ctx.reply(result);
-    } else {
-      return ctx.reply("Для начала нужно узнать с кем игра..");
-    }
-  }
-  if (ctx.message?.text === "/previous") {
-    let previous: IGame | undefined;
+			const result = `В последних ${numberOfMatches} матчах у Юве против ${anotherTeam.name} \n${juve.wins} побед ${juve.draws} ничей ${juve.losses} поражений `
+			return await ctx.reply(result)
+		} else {
+			return ctx.reply('Для начала нужно узнать с кем игра..')
+		}
+	}
+	if (ctx.message?.text === '/previous') {
+		let previous: IGame | undefined
 
-    if (previousId) {
-      const options = {
-        headers: { "X-Auth-Token": "1bb65d5d077f4ccba1280a3735cb9242" },
-      };
+		if (previousId) {
+			const options = {
+				headers: { 'X-Auth-Token': '1bb65d5d077f4ccba1280a3735cb9242' },
+			}
 
-      await fetch(
-        `https://api.football-data.org/v4/teams/${previousId}/matches?dateFrom=2023-08-29&?&dateTo=${today}&?limit=1`,
+			await fetch(
+				`https://api.football-data.org/v4/teams/${previousId}/matches?dateFrom=2023-08-29&?&dateTo=${today}&?limit=1`,
 
-        options
-      )
-        .then((response) => response.json())
-        .then(
-          (response) =>
-            (previous = response.matches[response.matches.length - 1])
-        );
-    }
+				options
+			)
+				.then(response => response.json())
+				.then(
+					response => (previous = response.matches[response.matches.length - 1])
+				)
+		}
 
-    if (previous?.matchday) {
-      const { away, home } = previous?.score?.fullTime;
-      const { matchday, awayTeam, competition, homeTeam, utcDate } = previous;
+		if (previous?.matchday) {
+			const { away, home } = previous?.score?.fullTime
+			const { matchday, awayTeam, competition, homeTeam, utcDate } = previous
 
-      const message = `${competition.name} - ${matchday} тур\n${formatedDate(
-        utcDate.toString().split("T")[0]
-      )}\n${homeTeam.name} ${home} - ${away} ${awayTeam.name}`;
+			const message = `${competition.name} - ${matchday} тур\n${formatedDate(
+				utcDate.toString().split('T')[0]
+			)}\n${homeTeam.name} ${home} - ${away} ${awayTeam.name}`
 
-      return ctx.reply(message);
-    } else {
-      return ctx.reply("Для начала нужно узнать с кем игра..");
-    }
-  }
-  await ctx.reply(`Очень интересно... 🤔, ничего не понял `);
-  await ctx.replyWithPhoto(
-    "https://assets-cms.thescore.com/uploads/image/file/473233/w1280xh966_GettyImages-1235259897.jpg?ts=1632233179"
-  );
-  return next();
-};
+			return ctx.reply(message)
+		} else {
+			return ctx.reply('Для начала нужно узнать с кем игра..')
+		}
+	}
+	await ctx.reply(`Очень интересно... 🤔, ничего не понял `)
+	await ctx.replyWithPhoto(
+		'https://assets-cms.thescore.com/uploads/image/file/473233/w1280xh966_GettyImages-1235259897.jpg?ts=1632233179'
+	)
+	return next()
+}
 
-bot.on("message", defaultReply, (ctx: Context) => {});
+bot.on('message', defaultReply, (ctx: Context) => {})
 
-if (process.env.NODE_ENV === "production") {
-  // Use Webhooks for the production server
-  const app = express();
-  const router = Router();
-  app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
+if (process.env.NODE_ENV === 'production') {
+	// Use Webhooks for the production server
+	const app = express()
+	const router = Router()
+	app.use(express.json())
+	app.use(webhookCallback(bot, 'express'))
 
-  router.use("/reminders", (req, res) => {
-    try {
-      bot.on("message", (ctx: Context) => {
-        console.log();
+	router.use('/reminders', (req, res) => {
+		try {
+			bot.on('message', (ctx: Context) => {
+				console.log()
 
-        return RemindersGame(
-          {
-            game,
-            today: today || new Date().toISOString().split("T")[0],
-          },
-          ctx
-        );
-      });
-      bot.drop;
-    } catch (error) {
-      console.log(error);
-    }
-  });
+				return RemindersGame(
+					{
+						game,
+						today: today || new Date().toISOString().split('T')[0],
+					},
+					ctx
+				)
+			})
+			bot.drop
+		} catch (error) {
+			console.log(error)
+		}
+	})
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
+	const PORT = process.env.PORT || 3000
+	app.listen(PORT, () => {
+		console.log(`Bot listening on port ${PORT}`)
+	})
 } else {
-  // Use Long Polling for development
-  bot.start();
+	// Use Long Polling for development
+	bot.start()
 }
